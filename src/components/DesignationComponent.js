6fd19316b05c771c7b26a35ed0b73e692e95584c
Pratibhaverma16 <62152179+Pratibhaverma16@users.noreply.github.com>
2021-08-25 21:25:00 +0530

    updated designation

--- src/components/DesignationComponent.js
+++ src/components/DesignationComponent.js
@@ -1,14 +1,44 @@
-import React, { Component } from "react";
+import React, { Component ,useState} from "react";
+import { FormControl } from "react-bootstrap";
+import Axios from 'axios';
 
-export default class Designation extends Component {
-    render() {
+
+
+
+ const  Designation=() => { 
+     const [designation,setDesignation]=useState("");
+     const handlechange=(e) =>{
+       setDesignation(e.target.value) 
+     }
+
+     const handlesubmit=(e) =>{
+         e.preventDefault()
+        console.log(designation)
+      }
+
+
+
+  const add = () =>{
+ Axios.post('http://localhost:3001/designation',{
+     designation:designation
+
+ }).then(() =>{
+
+    console.log("success");
+  });
+};
+
+
+
+
+
         return (
-            <form>
+            <form onSubmit={handlesubmit}  >
                 <h3>Add designation</h3>
 
                 <div className="form-group">
                     <label>Designation Name</label>
-                    <input type="email" className="form-control" placeholder="Enter" />
+                    <input type="text" value={designation}  onChange={handlechange}   className="form-control" placeholder="Enter" />
                 </div>
 
                 
@@ -20,11 +50,11 @@ export default class Designation extends Component {
                     </div>
                 </div>
 
-                <button type="submit" className="btn btn-primary btn-block">Add</button>
+                <button type="submit"   onClick={add}     className="btn btn-primary btn-block">Add</button>
                 <p className="forgot-password text-right">
                     Forgot <a href="#">password?</a>
                 </p>
             </form>
         );
     }
-}
+export default Designation
